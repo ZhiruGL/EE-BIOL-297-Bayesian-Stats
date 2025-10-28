a3a8d1d2a4c4cf949370a12c6bdb32a6acaee45c
ZhiruGL <zguoli3@g.ucla.edu>
2025-10-28 12:24:18 -0700

    done

---
+++ Revision.R
@@ -0,0 +1,14 @@
+How to name the component of a linear regression model
+
+Explain the differences and interpretation between frequentists and Bayesian statistics
+
+Explain each component fo Bayes theorem
+
+Should be able to choose an appropriate conjugate priors
+
+Should be able to take a dag and go from that to the equation and do the opposite as well
+
+Should know how to formulate a MCMC process
+
+Should be able to interprete the output of the MCMC model
+
